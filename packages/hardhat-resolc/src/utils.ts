6d531cfdd5381b6a9d7c1613d1ab82af11eda54e
bee344 <alberto.penayo@parity.io>
2024-12-12 15:04:40 -0300

    fixed config updater

--- packages/hardhat-resolc/src/utils.ts
+++ packages/hardhat-resolc/src/utils.ts
@@ -1,5 +1,8 @@
 import { Artifact } from "hardhat/types";
 import { ARTIFACT_FORMAT_VERSION } from "hardhat/internal/constants";
+import { ResolcConfig, SolcConfigData } from "./types";
+import { COMPILER_RESOLC_NEED_EVM_CODEGEN } from "./constants";
+import chalk from 'chalk';
 
 export function getArtifactFromContractOutput(
   sourceName: string,
@@ -27,6 +30,28 @@ export function getArtifactFromContractOutput(
   };
 }
 
+export function getVersionComponents(version: string): number[] {
+  const versionComponents = version.split('.');
+  return [parseInt(versionComponents[0], 10), parseInt(versionComponents[1], 10), parseInt(versionComponents[2], 10)];
+}
+
+export function updateDefaultCompilerConfig(solcConfigData: SolcConfigData, resolc: ResolcConfig) {
+  const compiler = solcConfigData.compiler;
+
+  const settings = compiler.settings || {};
+
+  compiler.settings = { ...settings, optimizer: { ...resolc.settings.optimizer }, evmVersion: resolc.settings.evmVersion };
+
+  resolc.settings.forceEVMLA = resolc.settings.forceEVMLA || false;
+
+  const [major, minor] = getVersionComponents(compiler.version);
+  if (major === 0 && minor < 8) {
+      console.warn(chalk.blue(COMPILER_RESOLC_NEED_EVM_CODEGEN));
+      compiler.settings.forceEVMLA = true;
+  }
+
+  delete compiler.settings.metadata;
+}
 
 export function pluralize(n: number, singular: string, plural?: string) {
   if (n === 1) {
