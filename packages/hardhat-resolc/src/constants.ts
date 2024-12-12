4453ac431d7e670768a63fd8a3b61db76eb72ce2
bee344 <alberto.penayo@parity.io>
2024-12-12 15:05:02 -0300

    fixed default config

--- packages/hardhat-resolc/src/constants.ts
+++ packages/hardhat-resolc/src/constants.ts
@@ -9,13 +9,15 @@ export const COMPILER_MIN_LLVM_VERSION = '18.1.4';
 export const RESOLC_COMPILER_PATH_VERSION = 'local_or_remote';
 export const TASK_UPDATE_SOLIDITY_COMPILERS = 'compile:update-solidity-compilers';
 
+export const COMPILER_RESOLC_NEED_EVM_CODEGEN = `Yul codegen is only supported for solc >= 0.8. Flag forceEVMLA will automatically be set to true by default.`;
+
 export const defaultResolcConfig: ResolcConfig = {
     version: 'latest',
     compilerSource: 'remix',
     settings: {
         optimizer: {
             enabled: true,
-            runs: '200',
+            runs: 200,
         },
     },
 };
