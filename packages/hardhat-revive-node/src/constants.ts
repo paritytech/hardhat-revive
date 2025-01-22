a9525a747b99af60447378d171e137e60d332ad2
bee344 <alberto.penayo@parity.io>
2025-01-22 19:36:54 -0300

    added prefunded accounts

--- packages/hardhat-revive-node/src/constants.ts
+++ packages/hardhat-revive-node/src/constants.ts
@@ -13,11 +13,23 @@ export const MAX_PORT_ATTEMPTS = 10;
 export const PORT_CHECK_DELAY = 500;
 export const RPC_ENDPOINT_PATH = 'eth_chainId';
 
-export const POLKAVM_TEST_NODE_NETWORK_NAME = 'hardhat';
+export const POLKAVM_TEST_NODE_NETWORK_NAME = 'Kitchensink';
 
 export const BASE_URL = `http://127.0.0.1`;
 export const NETWORK_ACCOUNTS = {
     REMOTE: 'remote',
+    POLKAVM: [
+        '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133',
+        '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5b0000a702133',
+        '0x5fb92c48bebcd6e98884f76de468fa3f6278f880713595d45af5b0000a702133',
+        '0x5fb92d6de468fa3f6278f8807c48bebc13595d45af5b000e98884f760a702133',
+        '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc130a702133595d45af5b000',
+        '0x5fb92d6e98884f76de468fa3f6275af5b0000a7021bc338f8807c48be13595d4',
+        '0x521338f8807c48befb92d6e98884f76de468fa3f6275af5b0000a70bc13595d4',
+        '0x5fb92d6e9884f768de468fa3f6275af5b0000a7021338f8807c48bebc13595d4',
+        '0x5fb92d6de468fa3f6275af5b0000a7021338f8807c48bebc13595d46e98884f7',
+        '0x5fb92d6e98884f76de468fa3f6275af5b0000a7021338f8807c48be3595d4bc1',
+    ]
 };
 export const NETWORK_GAS = {
     AUTO: 'auto',
