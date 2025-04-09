dafdabb5bc811c8da6edb741429a38870011e440
bee344 <alberto.penayo@parity.io>
2025-04-09 12:19:53 -0300

    cleanup

--- packages/hardhat-resolc/src/constants.ts
+++ packages/hardhat-resolc/src/constants.ts
@@ -6,15 +6,10 @@ export const DEFAULT_TIMEOUT_MILISECONDS = 30000;
 
 export const COMPILER_RESOLC_NEED_EVM_CODEGEN = `Yul codegen is only supported for solc >= 0.8. Flag forceEVMLA will automatically be set to true by default.`;
 
-export const defaultRemixResolcConfig: ResolcConfig = {
+export const defaultNpmResolcConfig: ResolcConfig = {
     version: 'latest',
-    compilerSource: 'remix',
-    settings: {
-        optimizer: {
-            enabled: true,
-            runs: 200,
-        },
-    },
+    compilerSource: 'npm',
+    settings: {},
 };
 
 export const defaultBinaryResolcConfig: ResolcConfig = {
